382c788800d1bc2fb920329d5cf842011ae7d4f6
DevTyagi610 <devtyagi610.com>
2024-08-01 23:53:45 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,9 +1,11 @@
 import os
 import sys
+import pandas as pd
+
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
-import pandas as pd
+from src.components.model_trainer import ModelTrainer , ModelTrainerConfig
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -44,4 +46,6 @@ if __name__ == "__main__":
     train_data , test_data = obj.initiate_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data , test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data , test_data)
+    modeltrainer = ModelTrainer()
+    print (modeltrainer.initiate_model_trainer(train_arr, test_arr))
